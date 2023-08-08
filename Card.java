8918cb23b75a651eb4482f450077f5bc2d013556
Yatharth Gureja <133058036+gurejay@users.noreply.github.com>
2023-08-08 14:47:32 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,18 +1,14 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ca.sheridancollege.project;
+package blackjackgame;
 
 /**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
- * @author Yatharth Gureja
- July 9
+ * @author hp
  */
-public abstract class Card {
+public class Card {
     private String rank;
     private String suit;
 
@@ -21,6 +17,16 @@ public abstract class Card {
         this.suit = suit;
     }
 
-    // Getters and setters for rank and suit
-}
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
